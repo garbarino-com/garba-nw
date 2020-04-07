7dbfff6e03cb8d7c2371abf0efdc2801ba1d0f5c
jnrch <jonathanrojas31@gmail.com>
2020-04-07 12:39:47 -0300

    actualizacion flujo compra

--- tests/flujoCompra.js
+++ tests/flujoCompra.js
@@ -1,51 +1,78 @@
-module.exports = {
-    'Buscar Heledera': function (browser) {
+describe('Realizar la compra de una heladera con retiro en sucursal y tarjeta de débito', function() {
+    before(function(browser) {
+      homepage = browser.page.home_PO();
+      search =  browser.page.search_PO();
+      producto = browser.page.producto_PO();
+      compraEntrega = browser.page.compraEntrega_PO();
+      compraFinanciacion = browser.page.compraFinanciacion_PO();
+      compraFacturacion = browser.page.compraFacturacion_PO();
+    });
+  
+    test('Ingreamos a la web de garbarino.com', () => {
+        homepage.navigate()
+    });
 
-        let home = browser.page.home_PO();
-        let search = browser.page.search_PO();
-        let producto = browser.page.producto_PO();
-        let compraEntrega = browser.page.compraEntrega_PO();
-        let compraFinanciacion = browser.page.compraFinanciacion_PO();
-        let compraFacturacion = browser.page.compraFacturacion_PO();
+    test('Realizamos la búsqueda de heladeras', () => {
+        homepage.assert.visible('@searchInput')
+        homepage.setValue('@searchInput', 'Heladeras')
+        homepage.clickSearchButton()
+    })
+    
+    test('Seleccionamos la primera heladera del listado', () => {
+        search.clickFirstItem()
+        .pause(2000)
+    })
 
-        home.navigate()
-            //.assert.title('Google')
-            .assert.visible('@searchInput')
-            .setValue('@searchInput', 'Heladeras')
-            .clickSearchButton()
+    test('Procedemos a realizar la compra de la heladera', () => {
+        producto.clickPurchaseButton()
+        .pause(2000)
+        producto.clickContinuarCarritoButton()
+    })
+    
+    test('Seleccionamos el barrio y/o localidad', () => {
+        compraEntrega.assert.visible('@city')
+        compraEntrega.setValue('@city', 'Villa Crespo, Buenos Aires, Ciudad de Buenos Aires')
+        compraEntrega.clickSuggestionCity()
+        .pause(5000)
+        compraEntrega.clickRetiroSucursal()
+        .pause(1000)
+        compraEntrega.clickSeleccionarSucursal()
+        compraEntrega.clickContinuarPickUpButton()
+        .pause(5000)
+    })
+
+    test('Seleccionamos los métodos de pago', () => {
+        compraFinanciacion.clickFormMetodosPago()
+        .pause(2000)
+        compraFinanciacion.clickSeleccionarTarjetaDebito()
+        .pause(1000)
+        compraFinanciacion.clickPresionarContinuarButtonMedioPago()
+    })
+
+    test('Completamos el formulario con los datos del cliente', () => {
+        compraFacturacion.setValue('@firstName', 'Jonathan')
+        compraFacturacion.setValue('@lastName', 'Rojas')
+        compraFacturacion.setValue('@documento', '9556097')
+        .click('select[id="month_select"] option[value="01"]')
+        compraFacturacion.click("@diaNacimiento",()=>{
+            compraFacturacion.click("option[value='31']");
+          });
+        compraFacturacion.click("@anioNacimiento",()=>{
+            compraFacturacion.click("option[value='1990']");
+          });
+        compraFacturacion.clickSelectGenero()
         
-            search.clickFirstItem()
-            .pause(5000)
-            
-            producto.clickPurchaseButton()
-            .pause(5000)
-            producto.clickContinuarCarritoButton()
-            .pause(5000)
-
-            compraEntrega.setValue('@city', 'Villa Crespo, Buenos Aires, Ciudad de Buenos Aires')
-            compraEntrega.clickSuggestionCity()
-            .pause(8000)
-            compraEntrega.clickRetiroSucursal()
-            .pause(5000)
-            compraEntrega.clickSeleccionarSucursal()
-            .pause(5000)
-            compraEntrega.clickContinuarPickUpButton()
-            .pause(5000)
-            compraFinanciacion.clickFormMetodosPago()
-            .pause(5000)
-            compraFinanciacion.clickSeleccionarTarjetaDebito()
-            .pause(5000)
-            compraFinanciacion.clickPresionarContinuarButtonMedioPago()
-            .pause(5000)
-            compraFacturacion.setValue('@firstName', 'Jonathan')
-            compraFacturacion.setValue('@lastName', 'Rojas')
-            compraFacturacion.setValue('@documento', '9556097')
-            //compraFacturacion.setValue('select[id="@diaNacimiento"]', '01')
-            compraFacturacion.clickSelectGenero()
-            .pause(5000)
-            // compraFinanciacion.clickAceptoTerminos()
-            // .pause(5000)
-            // compraFinanciacion.clickButtonContinue()
-            // .pause(5000)
-    }
-}
+        compraFacturacion.setValue('@codigoArea', '11')
+        compraFacturacion.setValue('@numeroTelefono', '69068083')
+        compraFacturacion.setValue('@email', 'jonathanrojas31@gmail.com')
+        compraFacturacion.setValue('@calle', 'Cuenca')
+        compraFacturacion.setValue('@altura', '3446')
+        compraFacturacion.setValue('@piso', '2')
+        compraFacturacion.setValue('@apartamento', 'D')
+        compraFacturacion.setValue('@codigoPostal', '1417')
+        
+        compraFacturacion.clickAceptoTerminos()
+        compraFacturacion.clickButtonContinue()
+        .pause(5000)
+    })
+  });
